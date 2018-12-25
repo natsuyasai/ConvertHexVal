b8d19cd6a681266e61804d293be5955c9e1916ca
natsuyasai <natsuyasai7777@gmail.com>
2018-12-25 21:46:16 +0900

    10進数変換修正

--- app/src/main/java/com/nyasai/converthexval/DecConverter.kt
+++ app/src/main/java/com/nyasai/converthexval/DecConverter.kt
@@ -7,7 +7,18 @@ class DecConverter(convertValueStr: String, isSigned: Boolean) : ValueConverter(
      * 変換データ確認
      */
     override fun chkConvertText(str: String): Boolean {
-        return (!str.isNullOrEmpty() && (str.toLong() < Int.MAX_VALUE) && (str.toLong() > Int.MIN_VALUE));
+        if(!str.isNullOrEmpty()) {
+            // 符号ありの場合
+            if(this.mIsSigned){
+                if(str.toLong() <= Int.MAX_VALUE && str.toLong() >= Int.MIN_VALUE)
+                    return true;
+            }
+            else{
+                if(str.toLong() <= 0xFFFFFFFF)
+                    return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -15,7 +26,7 @@ class DecConverter(convertValueStr: String, isSigned: Boolean) : ValueConverter(
      */
     override fun execConvert(): String {
         // 16進数へ変換
-        var str = Integer.toHexString(this.mConvertValueStr.toInt());
-        return "0x" + str.toUpperCase();
+        var str = java.lang.Long.toHexString(this.mConvertValueStr.toLong());
+        return str.toUpperCase();
     }
 }
--- app/src/main/java/com/nyasai/converthexval/HexConverter.kt
+++ app/src/main/java/com/nyasai/converthexval/HexConverter.kt
@@ -7,7 +7,7 @@ class HexConverter(convertValueStr: String, isSigned: Boolean) : ValueConverter(
      */
     override fun chkConvertText(str: String): Boolean {
         // 有効なデータであるか
-        if(!str.isNullOrEmpty() && str.indexOf("0x",0) != -1 && str.length >=3)
+        if(!str.isNullOrEmpty() && str.length >=1)
         {
             // 16進数の有効な文字が
             for (i in 2..(this.mConvertValueStr.length-1)) {
@@ -26,21 +26,16 @@ class HexConverter(convertValueStr: String, isSigned: Boolean) : ValueConverter(
      * 変換処理実行
      */
     override fun execConvert(): String {
-        // 0xを削除した文字列を生成
-        var cnvStr = "";
-        for (i in 2..(this.mConvertValueStr.length-1)){
-            cnvStr += this.mConvertValueStr[i];
-        }
         // 10進数へ変換
-        var value = cnvStr.toLong(16);
+        var value = this.mConvertValueStr.toLong(16);
         // 符号bit確認
-        var tmpValue = this.mConvertValueStr[2].toString().toInt(16);
+        var tmpValue = this.mConvertValueStr[0].toString().toInt(16);
         var sinBit = tmpValue and 0x08;
         // unsignedかつ符号bitが1のため，変換実施
         if (sinBit != 0 && this.mIsSigned != false) {
             // 負数のため，10進数へ戻す
             var xorHexStr =""
-            for (i in 2..(this.mConvertValueStr.length-1)){
+            for (i in 0..(this.mConvertValueStr.length-1)){
                 xorHexStr += "F";
             }
             var xorValue = xorHexStr.toLong(16);
